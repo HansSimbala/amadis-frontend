3453fc95aa69e281dd53712621a9036fd0f14e22
hanssimbala <hans31_1998@hotmail.com>
2020-12-01 18:54:05 -0500

    update slug

--- src/app/permissions.ts
+++ src/app/permissions.ts
@@ -1,10 +1,12 @@
 export const permissions = [
     {
       name: "Listar Clientes",
+      slug: "/customers/list",
       description: "Listar CLientes."
     },
     {
       name: "Listar Pedidos",
+      slug: "/customers/list",
       description: "Listar Pedidos."
     }
   ];
