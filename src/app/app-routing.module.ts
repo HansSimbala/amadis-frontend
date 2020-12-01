275377400e0eed16edbe42321131353054dca324
hanssimbala <hans31_1998@hotmail.com>
2020-12-01 18:33:53 -0500

    update list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,11 @@ const routes: Routes = [
     path: '',
     loadChildren: () =>
       import('./home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'customers',
+    loadChildren: () =>
+      import('./customers/customers.module').then(m => m.CustomersModule)
   }
 ];
 
